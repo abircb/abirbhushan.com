e75e6b6286c726d1cf9bd08b57690a5fe7a28bcc
abir <abirchandrabhushan@gmail.com>
2020-07-23 01:20:24 +0530

    a more verbose deployment

--- deploy/run.sh
+++ deploy/run.sh
@@ -4,10 +4,11 @@ echo "Building ..."
 yarn build
 
 echo "Copying build files to main"
-cp -a ../dist ../abircb.github.io
+cp -a dist/. ../abircb.github.io
 cd ../abircb.github.io
 
 echo "Committing changes"
 git add .
+git status
 git commit -m "automated deploy"
 git push origin master
